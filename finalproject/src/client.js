9d710135f2dfcb15bb5b57d9ef505eb8dde1428c
taylor <fematr@oregonstate.edu>
2025-04-19 11:37:50 -0700

    Add supabase info to client file

--- finalproject/src/client.js
+++ finalproject/src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = 'https://your-supabase-url.supabase.co';
-const API_KEY = 'your-supabase-api-key';
+const URL = 'https://nfawzhyflwypaeacbseu.supabase.co';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mYXd6aHlmbHd5cGFlYWNic2V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwODYwMDEsImV4cCI6MjA2MDY2MjAwMX0.i04GG0X5HaHE2UMqebfSRcaQEl7VFLT6nl9b1U6ohn4';
 
 export const supabase = createClient(URL, API_KEY);
